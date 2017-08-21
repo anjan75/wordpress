459b7b9e41d16bd8561fe3997c9452142093aaf7
anjan <anjang987@gmail.com>
2017-08-20 21:15:28 -0500

    my changes on theme

--- wp-content/themes/twentyfifteen/header.php
+++ wp-content/themes/twentyfifteen/header.php
@@ -21,14 +21,15 @@
 	<?php wp_head(); ?>
 </head>
 
-<body <?php body_class(); ?>>
+<body <?php body_class();  ?>>
 <div id="page" class="hfeed site">
 	<a class="skip-link screen-reader-text" href="#content"><?php _e( 'Skip to content', 'twentyfifteen' ); ?></a>
 
 	<div id="sidebar" class="sidebar">
 		<header id="masthead" class="site-header" role="banner">
 			<div class="site-branding">
-				<?php
+				<?php 
+                echo "anjaneyulu";
 					twentyfifteen_the_custom_logo();
 
 					if ( is_front_page() && is_home() ) : ?>
